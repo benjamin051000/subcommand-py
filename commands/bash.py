4b3b85b75ddbc7887d011b471236abe309213ee8
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-03-27 18:08:54 -0400

    Fixed bash script

--- commands/bash.py
+++ commands/bash.py
@@ -1,13 +1,12 @@
 import click
-import sys
 import subprocess
 from pathlib import Path
 
-@click.command
-def bash():
+# See https://click.palletsprojects.com/en/stable/advanced/#forwarding-unknown-options
+@click.command(context_settings=dict(ignore_unknown_options=True))
+@click.argument("bash_args", nargs=-1, type=click.UNPROCESSED)
+def bash(bash_args: tuple[str, ...]) -> None:
     """Run a bash subprocess."""
     path_to_exe = Path("./commands/bash_script")
-    temp = [path_to_exe] + sys.argv[1:]
-    breakpoint()
+    temp = [path_to_exe] + list(bash_args)
     subprocess.run(temp)
-
